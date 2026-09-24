3d6eac85e790c4ffd5f6b17a674f67d465171355
Ananya <ananyap2505@gmail.com>
2026-09-24 22:38:40 +0530

    fix(deploy): update next config and workflow for static export

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/portfolio',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -12,3 +15,4 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
